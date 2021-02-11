11a874fbac21698bf3dbbe3f90022cfa2cadd7c5
Domantas Jadenkus <djadenkus@gmail.com>
2021-02-11 19:04:33 +0200

    fix some type error messages

--- ticc/src/compiler/typeck.rs
+++ ticc/src/compiler/typeck.rs
@@ -9,6 +9,7 @@ pub(crate) fn type_check(compilation: &Compilation, item: &mut ir::Item, scope: 
     let syntax = &item.syntax;
     let mut checker = TypeChecker {
         compilation,
+        scope: &scope,
         unifier: Unifier::default(),
         span_types: HashMap::new(),
         symbols: HashMap::new(),
@@ -212,7 +213,6 @@ impl Unifier {
             TySlot::Ty(_) => {}
             TySlot::Next(next) => {
                 self.taint(next);
-                self.slots[ty.0 as usize] = TySlot::Ty(Ty::Error);
             }
             TySlot::Infer => {
                 self.slots[ty.0 as usize] = TySlot::Ty(Ty::Error);
@@ -298,6 +298,7 @@ struct Ctor<'a> {
 
 struct TypeChecker<'a> {
     compilation: &'a Compilation,
+    scope: &'a Scope<'a>,
     unifier: Unifier,
     span_types: HashMap<Span, TyIdx>,
     symbols: HashMap<Span, ir::Symbol>,
@@ -316,8 +317,9 @@ impl<'a> TypeChecker<'a> {
     }
 
     fn print_type(&mut self, ty: TyIdx) -> String {
-        let mut printer = crate::api::info::TypePrinter {
+        let mut printer = TypePrinter {
             compilation: self.compilation,
+            scope: self.scope,
             name_cache: HashMap::new(),
             used_names: HashSet::new(),
         };
@@ -566,5 +568,92 @@ impl<'a> TypeChecker<'a> {
         if let Some(e) = expr.rhs() {
             self.check_expr(e, ty);
         }
+    }
+}
+
+// FIXME: unify with the one in crate::api::info
+#[derive(PartialEq, Eq, Hash, Clone, Copy)]
+struct Name<'a> {
+    name: &'a str,
+    idx: u32,
+}
+
+struct TypePrinter<'a> {
+    compilation: &'a Compilation,
+    scope: &'a Scope<'a>,
+    name_cache: HashMap<ir::Symbol, Name<'a>>,
+    used_names: HashSet<Name<'a>>,
+}
+
+impl<'a> TypePrinter<'a> {
+    fn print_type(&mut self, ty: &ir::Type, to: &mut String) {
+        self.print_type_prec(ty, 0, to);
+    }
+
+    // 0 - no parentheses
+    // 1 - parentheses on functions
+    // 2 - parentheses on type application
+    fn print_type_prec(&mut self, ty: &ir::Type, prec: u8, to: &mut String) {
+        match ty {
+            ir::Type::Int => to.push_str("int"),
+            ir::Type::Bool => to.push_str("bool"),
+            ir::Type::Named(t, a) => {
+                if prec >= 2 && a.len() > 0 {
+                    to.push('(');
+                }
+                let name = self.lookup_name(*t);
+                to.push_str(name.name);
+                if name.idx > 0 {
+                    use std::fmt::Write;
+                    write!(to, "{}", name.idx).unwrap();
+                }
+                for ty in a {
+                    to.push(' ');
+                    self.print_type_prec(ty, 2, to);
+                }
+                if prec >= 2 && a.len() > 0 {
+                    to.push(')');
+                }
+            }
+            ir::Type::Fn(a, b) => {
+                if prec >= 1 {
+                    to.push('(');
+                }
+                self.print_type_prec(a, 1, to);
+                to.push_str(" -> ");
+                self.print_type_prec(b, 0, to);
+                if prec >= 1 {
+                    to.push(')');
+                }
+            }
+            ir::Type::Rec => to.push_str("rec"),
+            ir::Type::Error => to.push_str("?"),
+            ir::Type::Infer => to.push_str("_"),
+            ir::Type::Folded(a, b) => {
+                to.push_str("{");
+                self.print_type_prec(a, 0, to);
+                to.push_str(" => ");
+                self.print_type_prec(b, 0, to);
+                to.push_str("}");
+            }
+        }
+    }
+
+    fn lookup_name(&mut self, symbol: ir::Symbol) -> Name<'a> {
+        if let Some(name) = self.name_cache.get(&symbol).copied() {
+            return name;
+        }
+
+        let def = self.scope.lookup_def(symbol);
+        let name = &self.compilation.src[def.span.source_range()];
+        let mut name = Name {
+            name,
+            idx: 0,
+        };
+        while self.used_names.contains(&name) {
+            name.idx += 1;
+        }
+        self.name_cache.insert(symbol, name);
+        name
     }
 }
